7158e99d0775492659993e59d3138c153aac925f
andschir <andschir@gmail.com>
2021-12-29 11:41:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Flask
+
+This repository contains the source code for my Flask web-application.
+
---
+++ flasky.py
@@ -0,0 +1,39 @@
+import os
+import click
+from flask_migrate import Migrate, upgrade
+from app import create_app, db
+from app.models import User, Follow, Role, Permission, Post, Comment
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Follow=Follow, Role=Role,
+                Permission=Permission, Post=Post, Comment=Comment)
+
+
+@app.cli.command()
+@click.argument('test_names', nargs=-1)
+def test(test_names):
+    """Run the unit tests."""
+    import unittest
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
+
+    # create or update user roles
+    Role.insert_roles()
+
+    # ensure all users are following themselves
+    User.add_self_follows()
