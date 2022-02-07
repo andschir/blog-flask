f10591a0906353973b76ee9d1b1c938ae9a17ae9
andschir <andschir@gmail.com>
2022-02-07 17:23:53 +0300

    improved modal; reworked showhide

--- app/static/scripts/add_ImageModals.js
+++ app/static/scripts/add_ImageModals.js
@@ -1,32 +1,84 @@
 $(document).ready(function(){
-  // Adding modals  to Post images
   function* enumerate(iterable) {
     let i = 0;
-
     for (const x of iterable) {
         yield [i, x];
         i++;
     }
   }
 
-  var imageList = document.querySelector(".post-body").getElementsByTagName('img');
+  var modalDialog = document.getElementById('imageModal').querySelector(".modal-dialog");
+  var postBodies = document.querySelectorAll(".post-body");
   var modalImage = document.getElementById('imageModal').querySelector(".modal-body").getElementsByTagName('img')[0];
 
-  for (var [i, image] of enumerate(imageList)) {
-    wrapper = document.createElement('div');
+  for (var [i, body] of enumerate(postBodies)) {
+    if (!!body.getElementsByTagName('img')[0]) {
+      imageList = body.getElementsByTagName('img');
 
-    parent = image.parentElement;
-    parent.replaceChild(wrapper, image);
-    wrapper.appendChild(image);
+      for (var [imgnumber, image] of enumerate(imageList)) {
+        wrapper = document.createElement('div');
+        parent = image.parentElement;
+        parent.replaceChild(wrapper, image);
+        wrapper.appendChild(image);
 
-    div = image.parentElement;
-    div.setAttribute('data-bs-target', "#imageModal");
-    div.setAttribute('data-bs-toggle', "modal");
-    div.classList.add('imageDiv');
+        div = image.parentElement;
+        div.setAttribute('data-bs-target', "#imageModal");
+        div.setAttribute('data-bs-toggle', "modal");
+        div.classList.add('imageDiv');
+      }
+    }
   }
 
+  $('#imageModal').on('hidden.bs.modal', function () {
+    document.querySelector(".modal-caption").textContent = '';
+  });
+
   $(".imageDiv").click(function() {
+    var imageWidth = this.getElementsByTagName('img')[0].naturalWidth;
+    var windowWidth = $(window).width();
+
+    if (windowWidth > 1200) {
+      if (imageWidth > 800) {
+        modalDialog.style.maxWidth = '800px';
+      } else {
+        modalDialog.style.maxWidth = imageWidth + 'px';
+      }
+      modalDialog.style.margin = '0 auto';
+    } else if (imageWidth > windowWidth) {
+      modalDialog.style.maxWidth = '100%';
+      modalDialog.style.margin = '0 auto';
+    } else {
+      modalDialog.style.maxWidth = imageWidth + 'px';
+      modalDialog.style.margin = '0 auto';
+    }
+
     src = this.getElementsByTagName('img')[0].getAttribute('src');
     modalImage.setAttribute('src', src);
+
+    if (!!this.parentElement.getElementsByTagName('figcaption')[0]){
+      document.querySelector(".modal-caption").textContent = this.parentElement.getElementsByTagName('figcaption')[0].textContent;
+    }
+  });
+
+  function forceDownload(url) {
+    var fileName = url.split('/').pop();
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "blob";
+    xhr.onload = function(){
+        var urlCreator = window.URL || window.webkitURL;
+        var imageUrl = urlCreator.createObjectURL(this.response);
+        var tag = document.createElement('a');
+        tag.href = imageUrl;
+        tag.download = fileName;
+        document.body.appendChild(tag);
+        tag.click();
+        document.body.removeChild(tag);
+    }
+    xhr.send();
+  }
+
+  $('#imageModal .download').on('click', function () {
+    forceDownload(modalImage.getAttribute('src'));
   });
 });
--- app/static/scripts/showhide.js
+++ app/static/scripts/showhide.js
@@ -1,19 +1,50 @@
-$(document).ready(function(){
-  $(".showhide a").on("click", function() {
-    var $this = $(this);
-    var parent = $this.parent().prev()[0]
-    var content = $this.parent().prev().children()[0]
-    var linkText = $this.text().toUpperCase();
+$(document).ready(function() {
+    var em = parseFloat(getComputedStyle($(".post-body div").parent()[0]).fontSize);
+    var divheight = $(".post-body div").children().parent()[0].clientHeight/em;
+    var divs = document.querySelectorAll('div.post-body-realsize');
+    var showhides = document.querySelectorAll('div.showhide');
+    var needed_height;
 
-    if (parent.clientHeight < content.clientHeight) {
-      parent.style.height = content.clientHeight + 10 + "px";
-      linkText = "Скрыть";
-    } else {
-      parent.style.height = '5em';
-      linkText = "Показать полностью";
-    }
+    divs.forEach(function(div, divnumber) {
+      if (div.clientHeight/em > divheight) {
+            showhides[divnumber].children[0].text = 'Показать полностью';
+      }
+      var sizer = div.parentElement;
+      if (!!div.getElementsByTagName('img')[0]) {
+        var firstImage = div.getElementsByTagName('img')[0];
+        firstImage.onload = function() {
+          var firstImageSize = firstImage.parentElement.clientHeight;
+        }
+        sizer.setAttribute('style','transition: 0s');
+        // TODO: Value in px is hardcoded. need container for showhide and post-edited
+        sizer.style.height = (firstImage.height + 60) + 'px';
+      }
+    });
 
-    $this.text(linkText);
-    event.preventDefault()
-  });
+    $(".showhide a").on("click", function() {
+      var $this = $(this);
+      var parent = $this.parent().prev()[0]
+      var content = $this.parent().prev().children()[0]
+      var linkText = $this.text().toUpperCase();
+
+      if (!!content.getElementsByTagName('img')[0]) {
+        var firstImage = content.getElementsByTagName('img')[0];
+        firstImage.onload = function() {
+          var firstImageSize = firstImage.parentElement.clientHeight;
+        }
+        parent.setAttribute('style','');
+        needed_height = (firstImage.height + 60) + 'px';
+      }
+
+      if (parent.clientHeight < content.clientHeight) {
+        parent.style.height = content.clientHeight + 10 + "px";
+        linkText = "Скрыть";
+      } else {
+        parent.style.height = needed_height;
+        linkText = "Показать полностью";
+      }
+
+      $this.text(linkText);
+      event.preventDefault()
+    });
 });
